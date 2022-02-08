a7a853c919d1ecaed71e339edd381fdaa0b1c450
Anna-tay <lightanna24@gmail.com>
2022-02-08 10:44:01 -0700

    for the terminal

--- unit03-jumper/UI.cs
+++ unit03-jumper/UI.cs
@@ -2,20 +2,45 @@ using System;
 
 namespace unit03_jumper
 {
+    /// <summary>
+        /// get imput and prints all outputs
+        /// </summary>
     class UI
     {
-        //---------------------------------------------------------------------
-        // Member Variables
-        //---------------------------------------------------------------------
-        
 
         //---------------------------------------------------------------------
         // Constructors
         //---------------------------------------------------------------------
+        public UI()
+        {
 
+        }
 
         //---------------------------------------------------------------------
         // Member Functions
         //---------------------------------------------------------------------
+        
+        /// get input from the user 
+        public string UserGuess()
+        {
+            Console.WriteLine("Guess a letter [a-z]: ");
+            return Console.ReadLine();
+        }
+        /// draws our guess board 
+        public void DrawBoard(list<string> line)
+        {
+            Console.WriteLine(line);
+        }
+
+
+        ///draws our jumper dude
+        public void DrawJumper(List<string> jumper)
+        {
+            Console.WriteLine(jumper);
+        }
+
+
+
+
     }
 }
