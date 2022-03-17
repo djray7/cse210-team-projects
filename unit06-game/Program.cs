420c7add6b67035db2a85e1d6df6dc099db4ad2e
Anna-tay <lightanna24@gmail.com>
2022-03-17 09:31:50 -0600

    to pull

--- unit06-game/Program.cs
+++ unit06-game/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Unit06.Game.Services;
+using Unit06.Game;
 
-namespace unit06_game
+
+namespace Unit06
 {
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create the cast
+            //Cast cast = new Cast();
+//
+            ////cast.AddActor("food", new Food());
+            //cast.AddActor("snake", new Snake(300, Constants.RED, Constants.YELLOW));
+            //cast.AddActor("snake", new Snake(600, Constants.WHITE, Constants.GREEN));
+            //cast.AddActor("player1", new Player(1, 0));
+            //cast.AddActor("player2", new Player(2, 830));
+
+            // create the services
+            //KeyboardService keyboardService = new KeyboardService();
+            VideoServices videoService = new VideoServices(false);
+           
+            //Director director = new Director(videoService);
+            //// create the script
+            //Script script = new Script();
+            ////script.AddAction("input", new ControlActorsAction(keyboardService, director));
+            //script.AddAction("input", new ControlActorsAction(keyboardService));
+            //script.AddAction("update", new MoveActorsAction());
+            //script.AddAction("update", new HandleCollisionsAction());
+            //script.AddAction("output", new DrawActorsAction(videoService));
+//
+            //// start the game
+            //Director director = new Director(videoService);
+            //director.StartGame(cast, script);
+            // bool isRDown = false;
+            // do
+            // {
+            //     isRDown = false;
+            //     isRDown = director.StartGame(cast, script, keyboardService);
+            //     if (isRDown == false)
+            //     {
+            //         break;
+            //     }
+            // } while (isRDown);          
         }
     }
 }
