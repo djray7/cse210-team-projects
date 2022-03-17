2806e5b9b1911ba94d5d43304bb39f09173db2cc
Anna-tay <lightanna24@gmail.com>
2022-03-17 09:41:24 -0600

    to pull

--- unit06-game/Game copy/Casting/Rectangle.cs
+++ unit06-game/Game copy/Casting/Rectangle.cs
@@ -5,34 +5,34 @@ namespace Unit06.Game.Casting
     /// </summary>
     public class Rectangle
     {
-        private Point position;
-        private Point size;
+        // private Point position;
+        // private Point size;
         
-        /// <summary>
-        /// Constructs a new instance of Rectangle.
-        /// </summary>
-        public Rectangle(Point position, Point size)
-        {
-            this.position = position;
-            this.size = size;
-        }
+        // /// <summary>
+        // /// Constructs a new instance of Rectangle.
+        // /// </summary>
+        // public Rectangle(Point position, Point size)
+        // {
+        //     this.position = position;
+        //     this.size = size;
+        // }
 
-        /// <summary>
-        /// Gets the position.
-        /// </summary>
-        /// <returns>The position.</returns>
-        public Point GetPosition()
-        {
-            return position;
-        }
+        // /// <summary>
+        // /// Gets the position.
+        // /// </summary>
+        // /// <returns>The position.</returns>
+        // public Point GetPosition()
+        // {
+        //     return position;
+        // }
 
-        /// <summary>
-        /// Gets the size.
-        /// </summary>
-        /// <returns>The size.</returns>
-        public Point GetSize()
-        {
-            return size;
-        }
+        // /// <summary>
+        // /// Gets the size.
+        // /// </summary>
+        // /// <returns>The size.</returns>
+        // public Point GetSize()
+        // {
+        //     return size;
+        // }
     }
 }
